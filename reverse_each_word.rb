bc96ff7a01203b0d10b9f114f3b41b9ebb4b18fe
alice T <alicewtien@gmail.com>
2018-09-02 21:33:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,18 @@
+#use .each - passed the test
+# def reverse_each_word(sentence)
+#  words = sentence.split(" ") #convert the string into an array
+#  reversed = [] #empty array for the reversed words
+#  words.each do |word| #reverse each word individually with .each
+#    reversed << word.reverse! #add the reversed words into the reversed array
+#  end
+#  reversed.join(" ") #convert the array back into a string
+#end
+
+#use .collect
 def reverse_each_word(sentence)
   words = sentence.split(" ") #convert the string into an array
-  reversed = [] #empty array for the reversed words
-  words.each do |word| #reverse each word individually with .each
-    reversed << word.reverse! #add the reversed words into the reversed array
+  words.collect do |word| #reverse each word individually with .collect
+    word.reverse! #add the reversed words into the reversed array
   end
-  reversed.join(" ") #convert the array back into a string
-
-
+  words.join(" ") #convert the array back into a string
 end
