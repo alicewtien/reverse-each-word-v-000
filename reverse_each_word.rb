1638591c845279405fcc46ab0495e00e1ac4c3fe
alice T <alicewtien@gmail.com>
2018-09-02 20:21:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,12 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
+  reversed = []
+  words.each do |word|
+    until word.length == 0
+      reversed << word.reverse!
+    end
+  end
+  reversed
 
 
 end
